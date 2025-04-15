ca8282cb76c0bb9f207d0767bb9a76b42c5222fa
mhama <ham.lua@gmail.com>
2025-04-15 18:01:11 +0900

    Update ColorSliderImage.cs

--- Packages/com.judahperez.hsvcolorpicker/UI/ColorSliderImage.cs
+++ Packages/com.judahperez.hsvcolorpicker/UI/ColorSliderImage.cs
@@ -15,6 +15,7 @@ namespace HSVPicker
         public Slider.Direction direction;
 
         private RawImage image;
+        private Texture2D initialTexture;
 
         private RectTransform rectTransform
         {
@@ -27,6 +28,7 @@ namespace HSVPicker
         private void Awake()
         {
             image = GetComponent<RawImage>();
+            initialTexture = image.texture;
 
             if(Application.isPlaying)
                 RegenerateTexture();
@@ -52,8 +54,8 @@ namespace HSVPicker
 
         private void OnDestroy()
         {
-            if (image.texture != null)
-                Destroy(image.texture);
+            if (image.texture != null && image.texture != initialTexture)
+                DestroyImmediate(image.texture);
         }
 
         private void ColorChanged(Color newColor)
@@ -184,7 +186,7 @@ namespace HSVPicker
             texture.SetPixels32(colors);
             texture.Apply();
 
-            if (image.texture != null)
+            if (image.texture != null && image.texture != initialTexture)
                 Destroy(image.texture);
             image.texture = texture;
 
